d13b0c04b330513d2f99c636da66b548583e4cb3
Angelo Zamudio <avaz1301@gmail.com>
2016-05-05 22:28:18 -0400

    Huge update. Added about section to home page, completed menu page, set up router to switch between view properly

--- public/javascripts/functions.js
+++ public/javascripts/functions.js
@@ -1,29 +1,24 @@
 $(document).ready(function() {
-  
-  // $(window).scroll(function () {
-  //     //if you hard code, then use console
-  //     //.log to determine when you want the 
-  //     //nav bar to stick.  
-  //     console.log($(window).scrollTop())
-  //   if ($(window).scrollTop() > 280) {
-  //     $('#nav-contain').addClass('pos-fixed');
-  //   }
-  //   if ($(window).scrollTop() < 281) {
-  //     $('#nav-contain').removeClass('pos-fixed');
-  //   }
-  // });
 
 
-  $(window).scroll(function() {
-    console.log($(window).scrollTop());
-    if($(window).scrollTop > 280){
-      $('#nav-bar').addClass('nav-pos-fixed');
-      console.log("ADD");
-    }
-    if($(window).scrollTop < 281){
-      $('#nav-bar').removeClass('nav-pos-fixed');
-      console.log("remove");
-    }//if
-  })//scroll
+
+    $(window).scroll(function () {
+        
+    });//scroll func
+    
+    
+    
+
+
+
+  // $(window).scroll(function() {
+  //   console.log($(window).scrollTop());
+  //   if($(window).scrollTop() < 364){
+  //     $('#nav-bar').addClass('fixed-nav');
+  //   }
+  //   else if($(window).scrollTop() > 364){
+  //     $('#nav-bar').removeClass('fixed-nav');
+  //   }//if
+  // })//scroll
 
 });
--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,43 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', {
+router.get('/', function(req, res) {
+  res.render('about', {
   	title: 'Sushi Yasaka',
   	header: 'Menu',
-  	address: '226 West 72nd Street, New York ,11021',
-  	phone: "Phone : (212) 496-8460" });
+  	address: '251 W 72nd St, New York, NY, 10023, United States',
+  	phone: '(212) 496-8460',
+	email: 'sushiyasakanyc@gmail.com',
+    googlemaps:'https://www.google.com/maps/place/Sushi+Yasaka/@40.7796266,-73.9856146,17z/data=!4m8!1m2!3m1!2sSushi+Yasaka!3m4!1s0x0000000000000000:0x6df52b9e7310c3c5!8m2!3d40.779627!4d-73.9834209'
+    });
+
+});
+
+// router.get('/menu', function(req,res){
+//     res.render('menu');
+// });
+router.get('/index.hjs', function(req,res){
+    res.render('index', {
+        title: 'Sushi Yasaka',
+        header: 'Menu',
+        address: '251 W 72nd St, New York, NY, 10023, United States',
+        phone: '(212) 496-8460',
+        googlemaps:'https://www.google.com/maps/place/Sushi+Yasaka/@40.7796266,-73.9856146,17z/data=!4m8!1m2!3m1!2sSushi+Yasaka!3m4!1s0x0000000000000000:0x6df52b9e7310c3c5!8m2!3d40.779627!4d-73.9834209',
+        email: 'sushiyasakanyc@gmail.com'});
+});
+
+router.get('/about.hjs', function(req, res) {
+    res.render('about', {
+        title: 'Sushi Yasaka',
+        header: 'Menu',
+        address: '251 W 72nd St, New York, NY, 10023, United States',
+        googlemaps:'https://www.google.com/maps/place/Sushi+Yasaka/@40.7796266,-73.9856146,17z/data=!4m8!1m2!3m1!2sSushi+Yasaka!3m4!1s0x0000000000000000:0x6df52b9e7310c3c5!8m2!3d40.779627!4d-73.9834209',
+        phone: '(212) 496-8460',
+        email: 'sushiyasakanyc@gmail.com'});
+});
+
+router.get('/gallery.html', function(req, res){
+    res.render('gallery');
 });
 
 module.exports = router;
